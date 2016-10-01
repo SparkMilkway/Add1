86c494ed848b16686ae00d7de129bc4b1336fe88
Luzhen Huang <onespark@bu.edu>
2016-10-01 17:29:23 -0400

    Initial Commit

---
+++ Add1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Add1
+//
+//  Created by Spark Da Capo on 10/1/16.
+//  Copyright © 2016 LearnAppMaking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
